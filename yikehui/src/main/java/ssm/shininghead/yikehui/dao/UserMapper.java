f2035b554e9439f1522d1c50f410e230112950b2
Xu Wanqing <wanqxu@gmail.com>
2013-03-06 08:47:12 +0800

    create a new project

---
+++ yikehui/src/main/java/ssm/shininghead/yikehui/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package ssm.shininghead.yikehui.dao;
+
+import ssm.shininghead.yikehui.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ yikehui/src/main/java/ssm/shininghead/yikehui/service/UserService.java
@@ -0,0 +1,9 @@
+package ssm.shininghead.yikehui.service;
+
+import ssm.shininghead.yikehui.model.User;
+
+public interface UserService {
+
+	public User getUserByID(Integer id);
+	
+}
